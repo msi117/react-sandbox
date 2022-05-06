5cb10055d9dfe1a56681f5e3046fc7ef1716c3fc
Mir Sameer <mir.sameer@example.com>
2022-05-06 19:56:53 +0530

    useRef Example2 addes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import UseRefExample1 from "./components/UseRefExample1";
+import UseRefExample2 from "./components/UseRefExample2";
 function App() {
   return (
     <div>
-      <UseRefExample1 />
+      {/* <UseRefExample1 /> */}
+      <UseRefExample2 />
     </div>
   );
 }
--- src/components/UseRefExample1.jsx
+++ src/components/UseRefExample1.jsx
@@ -1,25 +1,35 @@
-import { useRef } from "react"
+import { useRef } from "react";
 
 function UseRefExample1() {
-    const inputRef = useRef() //useRef is used to update the element without rendering the components
-    const paraRef = useRef()
+  const inputRef = useRef(); //useRef is used to update the element without rendering the components
+  const paraRef = useRef();
 
-    const onSubmit = (e) => {
-        e.preventDefault()
-        console.log(inputRef.current.value);
-        inputRef.current.value = 'SAMEER'
-        inputRef.current.style.backgroundColor = 'red'
-    }
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(inputRef.current.value);
+    inputRef.current.value = "SAMEER";
+    inputRef.current.style.backgroundColor = "red";
+  };
   return (
     <div>
-        <form onSubmit={onSubmit}>
-            <label htmlFor="name" className="">Name</label>
-            <input type="text" ref={inputRef} name="name" id="name" className="form-control mb-2"/>
-            <button className="btn btn-primary">Submit</button>
-            <p ref={paraRef} onClick={() => inputRef.current.focus()  }>This is before submitting the form</p>
-        </form>
+      <form onSubmit={onSubmit}>
+        <label htmlFor="name" className="">
+          Name
+        </label>
+        <input
+          type="text"
+          ref={inputRef}
+          name="name"
+          id="name"
+          className="form-control mb-2"
+        />
+        <button className="btn btn-primary">Submit</button>
+        <p ref={paraRef} onClick={() => inputRef.current.focus()}>
+          This is before submitting the form
+        </p>
+      </form>
     </div>
-  )
+  );
 }
 
-export default UseRefExample1
+export default UseRefExample1;
